884bef72830cf5f335c4e5ee989c1fd9a934da9b
Mickey <mickey@Mickeys-MacBook-Pro.local>
2020-07-12 03:11:18 -0500

    updated

--- js_sandbox/3_2_project_files/js_sandbox/js_sandbox/app.js
+++ js_sandbox/3_2_project_files/js_sandbox/js_sandbox/app.js
@@ -1,50 +1,12 @@
-// let val;
-
-// val = document;
-// val = document.all;
-// val = document.all[2];
-// val = document.all.length;
-// val = document.head;
-// val = document.body;
-// val = document.doctype;
-// val = document.domain;
-// val = document.URL;
-// val = document.characterSet;
-// val = document.contentType;
-
-// val = document.forms;
-// val = document.forms[0];
-// val = document.forms[0].id;
-// val = document.forms[0].method;
-// val = document.forms[0].action;
-
-// val = document.links;
-// val = document.links[0];
-// val = document.links[0].id;
-// val = document.links[0].className;
-// val = document.links[0].classList[0];
-
-// val = document.images;
-
-// val = document.scripts;
-// val = document.scripts[2].getAttribute('src');
-
-// let scripts = document.scripts;
-
-// let scriptsArr = Array.from(scripts);
-
-// scriptsArr.forEach(function(script) {
-//   console.log(script.getAttribute('src'));
-// });
-
-// console.log(val);
 
 
+console.log(document.getElementById("task-title"))
+
+console.log(document.getElementById("task-title").id)
+console.log(document.getElementById("task-title").className)
+
+document.getElementById('task-title').style.background = 'black'
+document.getElementById('task-title').style.background = '#FFF'
+document.getElementById('task-title').style.padding = '5px'
 
 
-let val;
-
-val = document
-val = document.all
-
-console.log(val)
