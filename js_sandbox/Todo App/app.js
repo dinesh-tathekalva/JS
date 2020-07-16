e99661d0cc248bf76fb18261f0dd470a5298f984
Mickey <mickey@Mickeys-MacBook-Pro.local>
2020-07-16 14:43:42 -0500

    Calculator added

--- js_sandbox/Todo App/app.js
+++ js_sandbox/Todo App/app.js
@@ -133,8 +133,31 @@ function removeTask(e) {
     if (e.target.parentElement.classList.contains('delete-item')) {
         if (confirm('Are you sure?')) {
             e.target.parentElement.parentElement.remove()
+
+            //Remove from LS
+            removeTaskFromLocalStorage(e.target.parentElement.parentElement)
         }
     }
+}
+
+//Remove from LS
+function removeTaskFromLocalStorage(taskItem){
+    let tasks
+    if (localStorage.getItem('tasks') === null) {
+        tasks = []
+    }
+    else {
+        tasks = JSON.parse(localStorage.getItem('tasks'))
+    }
+    
+    tasks.forEach(function(task, index){
+        if(taskItem.textContent === task){
+            tasks.splice(index, 1)
+        }
+    })
+
+    localStorage.setItem('tasks', JSON.stringify(tasks))
+
 }
 
 //Clear Tasks
@@ -144,6 +167,13 @@ function clearTasks() {
     while (tasklist.firstChild) {
         tasklist.removeChild(tasklist.firstChild)
     }
+    //Clear from LS
+    clearTasksFromLocalStorage()
+}
+
+//Clearing tasks from LS
+function clearTasksFromLocalStorage(){
+    localStorage.clear()
 }
 
 //Filter Tasks
