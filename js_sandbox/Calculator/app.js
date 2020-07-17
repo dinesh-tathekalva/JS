c0b0aa83d0aa07c67c769b2bcb6878e867acd841
Mickey <mickey@Mickeys-MacBook-Pro.local>
2020-07-17 00:57:03 -0500

    updated

--- js_sandbox/Calculator/app.js
+++ js_sandbox/Calculator/app.js
@@ -1,7 +1,6 @@
 document.querySelector('#loan-form').addEventListener('submit', handleResults)
 
 function handleResults(e){
-
     //Hide results
     document.querySelector('#results').style.display = 'none'
 
@@ -15,7 +14,6 @@ function handleResults(e){
 
 function calculateResults(){
     console.log('Calculating')
-
     //UI variables
     const amount = document.querySelector('#amount')
     const interest = document.querySelector('#interest')
@@ -43,19 +41,15 @@ function calculateResults(){
 
         //Show results
         document.querySelector('#results').style.display = 'block'
-
-        
-
     }else{
         showError('Please check your numbers')
-
-        //hide loader
-        document.querySelector('#loading').style.display = 'none'
     }
-
 }
 
 function showError(error){
+     //hide loader
+     document.querySelector('#loading').style.display = 'none'
+
     //Create a div
     const errorDiv = document.createElement('div');
 
